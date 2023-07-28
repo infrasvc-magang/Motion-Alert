9ed67c7c3afe9d41b0d2de30303b3658734cf506
Putra Rafii <90172609+RoughTiime@users.noreply.github.com>
2023-07-28 16:19:16 +0700

    Update alertWhatsApp.py

--- alertWhatsApp.py
+++ alertWhatsApp.py
@@ -3,5 +3,5 @@ import datetime
 
 def alert():
     myobj = datetime.now()
-    pywhatkit.sendwhatmsg_to_group("KGJ5Jc0nRWnIEsIH3MhdCs", "Motion detected at " + str(myobj.hour) + ":"+ str(myobj.minute)+ ", please check." , myobj.hour , myobj.minute+1 , 10, True, 5) #group
-    pywhatkit.sendwhatmsg("+6285291612998", "test 1 2 3", myobj.hour , myobj.minute+1) #personal chat
+    pywhatkit.sendwhatmsg_to_group("Group_ID", "Motion detected at " + str(myobj.hour) + ":"+ str(myobj.minute)+ ", please check." , myobj.hour , myobj.minute+1 , 10, True, 5) #group
+    #pywhatkit.sendwhatmsg("Sender_Number", "Message", Hour , Minute, Delay, True/False, Close Tab) #personal chat
